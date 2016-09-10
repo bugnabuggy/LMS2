970d31961abd9cf3d98c59792a2b175cdb81f700
Anton Yakovets <jokerosky@gmail.com>
2016-09-10 23:48:39 +0600

    scss compilation and dependencies

--- src/LMS.Web/gulpfile.js
+++ src/LMS.Web/gulpfile.js
@@ -4,6 +4,8 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 var libs = ['./node_modules/core-js/client/shim.min.js',
             './node_modules/zone.js/dist/zone.js',
@@ -17,4 +19,17 @@ gulp.task("libs", function () {
 
 gulp.task('default', function () {
     // place code for your default task here
+});
+
+gulp.task('sass', function () {
+    return gulp.src([   
+                        './Assets/**/*.scss'
+    ])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concatCss("site.css"))
+      .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
